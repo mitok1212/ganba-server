51b7bada1f4794bc09adc078df0b0aba80d9af6c
mitok1212 <mitokatayama1231003@gmail.com>
2024-01-29 01:35:51 +0900

    studynote create

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_28_061035) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_28_140015) do
   create_table "frineds", charset: "utf8mb4", force: :cascade do |t|
     t.string "friendme", null: false
     t.string "friendyou", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "studynotes", charset: "utf8mb4", force: :cascade do |t|
+    t.date "date", null: false
+    t.time "studytime", null: false
+    t.string "subject", null: false
+    t.string "username", null: false
+    t.text "goal", null: false
+    t.text "studycontent", null: false
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
---
+++ backend/test/models/studynote_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class StudynoteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
