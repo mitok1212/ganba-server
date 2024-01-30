85788c414175a034c540ad7c085619cc122326a7
mitok1212 <mitokatayama1231003@gmail.com>
2024-01-31 01:10:56 +0900

    route create

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -18,4 +18,17 @@ Rails.application.routes.draw do
     post '/delete', to: 'friends#delete_friend'
     get '/search/:myself', to:'frinends#search'
   end
+
+  scope 'reaction' do
+    post '/add', to: 'reactions#add_reaction'
+    post '/update', to: 'reactions#update_reaction'
+    get '/search/:myname', to: 'reactions#search_reaction'
+  end
+
+  scope 'studynote' do
+    post '/add', to: 'studynotes#add_studynote'
+    post '/date', to: 'studynotes#date'
+    post '/time', to: 'studynotes#studytime'
+    post '/goal', to: 'studynotes#goal'
+  end
 end
