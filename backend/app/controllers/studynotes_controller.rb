54c9ae85444f5924799c5bea5ce814e6797e7e05
Ishigami100 <yamahaya5151@icloud.com>
2024-02-02 03:11:27 +0900

    呼び出し関数を変更

--- backend/app/controllers/studynotes_controller.rb
+++ backend/app/controllers/studynotes_controller.rb
@@ -3,7 +3,7 @@ class StudynotesController < ApplicationController
         Studynote.create(date: params[:date],studytime: params[:studytime],subject: params[:subject],username: params[:username],goal: params[:goal])
     end
     def show_studynote
-        studynote = Studynote.find_by(username: params[:username])
+        studynote = Studynote.where(username: params[:username])
         if studynote != nil
           render json: {message: "succeed",data: studynote}, status: :ok
         else
